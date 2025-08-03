b7a6ffb1142066d56495d74b3e4d7a0af36969cf
riham <riham91.alsiyabi@gmail.com>
2025-08-03 14:46:17 +0400

    Add Member model with Id and Name.

--- Models/Member.cs
+++ Models/Member.cs
@@ -8,6 +8,7 @@ namespace librarymanagementArchitectureRepository.Models
 {
     public class Member // Member model representing a library member
     {
+        // Unique identifier and name properties for the member
         public string Id { get; set; } // Unique identifier for the member
 
         public string Name { get; set; } // Name of the member
