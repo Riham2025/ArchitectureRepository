edfd18523623e0fde634ef34eadc136aab6fb5ed
riham <riham91.alsiyabi@gmail.com>
2025-08-03 14:41:28 +0400

    Availability status of the book

--- Models/Book.cs
+++ Models/Book.cs
@@ -11,5 +11,7 @@ namespace librarymanagementArchitectureRepository.Models
         public string Id { get; set; } // Unique identifier for the book
         public string Title { get; set; } // Title of the book
         public string Author { get; set; } // Author of the book
+
+        public bool IsAvailable { get; set; } = true; // Availability status of the book
     }
 }
