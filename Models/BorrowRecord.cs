acca63b4c9a97b5c11595e006a70f2a725ca95fd
riham <riham91.alsiyabi@gmail.com>
2025-08-03 14:48:25 +0400

    create new class call BorrowRecord

---
+++ Models/BorrowRecord.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace librarymanagementArchitectureRepository.Models
+{
+    public class BorrowRecord // BorrowRecord model representing a record of a book borrowed by a member
+    {
+        
+    }
+}
