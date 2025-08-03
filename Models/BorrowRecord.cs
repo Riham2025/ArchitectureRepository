15fcf9edea128547b2b86dbc0b2417d92d4d6bda
riham <riham91.alsiyabi@gmail.com>
2025-08-03 14:49:45 +0400

    Unique identifier for the member who borrowed the book

--- Models/BorrowRecord.cs
+++ Models/BorrowRecord.cs
@@ -9,6 +9,8 @@ namespace librarymanagementArchitectureRepository.Models
     public class BorrowRecord //BorrowRecord model representing a record of a book borrowed by a member
     {
         public string Id { get; set; } // Unique identifier for the borrow record
-        
+
+        public string MemberId { get; set; } // Unique identifier for the member who borrowed the book
+
     }
 }
