76f462bc1df9f9a8c7910f3bea3d418f7fd5d591
riham <riham91.alsiyabi@gmail.com>
2025-08-04 09:22:18 +0400

    Assign the provided file context to the repository

--- Repository/MemberRepository.cs
+++ Repository/MemberRepository.cs
@@ -15,7 +15,7 @@ namespace librarymanagementArchitectureRepository.Repository
 
         public MemberRepository(FileContext context) // Constructor to initialize the repository with a file context
         {
-            _context = context;
+            _context = context; // Assign the provided file context to the repository
             _members = _context.LoadMembers(); 
         }
         
