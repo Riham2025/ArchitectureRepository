2104ee115c48d743f26dd7dea0e326bc2510f840
riham <riham91.alsiyabi@gmail.com>
2025-08-04 09:21:07 +0400

    List to hold members in memory

--- Repository/MemberRepository.cs
+++ Repository/MemberRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using librarymanagementArchitectureRepository.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,7 @@ namespace librarymanagementArchitectureRepository.Repository
     class MemberRepository
     {
         private readonly FileContext _context; // File context for managing file operations
+
+        private List<Member> _members; // List to hold members in memory
     }
 }
