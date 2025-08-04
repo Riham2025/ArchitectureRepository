9598a6e2b531c2ad5105aa864c193990fdfc23cd
riham <riham91.alsiyabi@gmail.com>
2025-08-04 10:08:41 +0400

    create  IBookRepository

---
+++ Repository/IBookRepository.cs
@@ -0,0 +1,12 @@
+﻿using librarymanagementArchitectureRepository.Models;
+
+namespace librarymanagementArchitectureRepository.Repository
+{
+    public interface IBookRepository
+    {
+        void Add(Book book);
+        List<Book> GetAll();
+        Book GetById(string id);
+        void Update(Book book);
+    }
+}
