e9e4b8d44577f5764861e93e745e7716803fc83c
riham <riham91.alsiyabi@gmail.com>
2025-08-04 10:08:18 +0400

    create  IBorrowRecordRepository

---
+++ Repository/IBorrowRecordRepository.cs
@@ -0,0 +1,12 @@
+﻿using librarymanagementArchitectureRepository.Models;
+
+namespace librarymanagementArchitectureRepository.Repository
+{
+    public interface IBorrowRecordRepository
+    {
+        void Add(BorrowRecord record);
+        List<BorrowRecord> GetAll();
+        BorrowRecord GetByBookAndMember(string bookId, string memberId);
+        void Update(BorrowRecord record);
+    }
+}
