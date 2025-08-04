4964263092310725cf43b20c2f6e1eec63bc2c4f
riham <riham91.alsiyabi@gmail.com>
2025-08-04 10:08:01 +0400

    create IBorrowRecordRepository

---
+++ Repository/IMemberRepository.cs
@@ -0,0 +1,11 @@
+﻿using librarymanagementArchitectureRepository.Models;
+
+namespace librarymanagementArchitectureRepository.Repository
+{
+    public interface IMemberRepository
+    {
+        void Add(Member member);
+        List<Member> GetAll();
+        Member GetById(string id);
+    }
+}
