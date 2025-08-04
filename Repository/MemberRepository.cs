effa1af3a8386754d7afa3cf8fa5448e655afb3b
riham <riham91.alsiyabi@gmail.com>
2025-08-04 09:22:02 +0400

    Constructor to initialize the repository with a file context

--- Repository/MemberRepository.cs
+++ Repository/MemberRepository.cs
@@ -12,5 +12,12 @@ namespace librarymanagementArchitectureRepository.Repository
         private readonly FileContext _context; // File context for managing file operations
 
         private List<Member> _members; // List to hold members in memory
+
+        public MemberRepository(FileContext context) // Constructor to initialize the repository with a file context
+        {
+            _context = context;
+            _members = _context.LoadMembers(); 
+        }
+        
     }
 }
