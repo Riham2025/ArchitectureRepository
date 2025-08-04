1e056838ffffcc03927c6cde1af943ae94e2faad
riham <riham91.alsiyabi@gmail.com>
2025-08-04 11:02:05 +0400

    BookRepository instance to manage book data

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace librarymanagementArchitectureRepository
         {
             var fileContext = new FileContext(); // FileContext instance to manage file operations
 
-            var bookRepo = new BookRepository(fileContext);
+            var bookRepo = new BookRepository(fileContext); // BookRepository instance to manage book data
             var memberRepo = new MemberRepository(fileContext);
             var recordRepo = new BorrowRecordRepository(fileContext);
 
