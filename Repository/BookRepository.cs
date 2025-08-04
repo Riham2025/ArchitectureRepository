8909be8c3dc6cd8526c69c8bf7c5e98cf3cef961
riham <riham91.alsiyabi@gmail.com>
2025-08-04 09:07:28 +0400

    Load books from the file context into the repository's memory

--- Repository/BookRepository.cs
+++ Repository/BookRepository.cs
@@ -16,7 +16,7 @@ namespace librarymanagementArchitectureRepository.Repository
         public BookRepository(FileContext context) // Constructor to initialize the repository with a file context
         {
             _context = context; // Assign the provided file context to the repository
-            _books = _context.LoadBooks(); 
+            _books = _context.LoadBooks(); // Load books from the file context into the repository's memory
         }
         
     }
