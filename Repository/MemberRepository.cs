4586608d7e42423984e695b29d45b5a059af954c
riham <riham91.alsiyabi@gmail.com>
2025-08-04 15:05:35 +0400

    In-memory list to store members

--- Repository/MemberRepository.cs
+++ Repository/MemberRepository.cs
@@ -9,24 +9,21 @@ namespace librarymanagementArchitectureRepository.Repository
 {
     public class MemberRepository : IMemberRepository
     {
-        private readonly FileContext _context; // File context for managing file operations
+        private List<Member> _members; // In-memory list to store members
 
-        private List<Member> _members; // List to hold members in memory
-
-        public MemberRepository(FileContext context) // Constructor to initialize the repository with a file context
+        public MemberRepository()
         {
-            _context = context; // Assign the provided file context to the repository
-            _members = _context.LoadMembers(); // Load members from the file context into the repository's memory
+            _members = FileContext.LoadMembers();
         }
 
-        public List<Member> GetAll() => _members; // Method to get all members from the repository
+        public List<Member> GetAll() => _members;
 
-        public Member GetById(string id) => _members.FirstOrDefault(m => m.Id == id); // Method to get a member by its unique identifier
+        public Member GetById(string id) => _members.FirstOrDefault(m => m.Id == id);
 
-        public void Add(Member member) // Method to add a new member to the repository
+        public void Add(Member member)
         {
-            _members.Add(member); // Add the new member to the in-memory list of members
-            _context.SaveMembers(_members); //  Save the updated list of members to the file context
+            _members.Add(member);
+            FileContext.SaveMembers(_members);
         }
 
 
