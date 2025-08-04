c6124b9f36ba300409c9d2d3a388223548e3e547
riham <riham91.alsiyabi@gmail.com>
2025-08-04 09:02:56 +0400

    Method to get a book by its unique identifier

--- Repository/BookRepository.cs
+++ Repository/BookRepository.cs
@@ -10,5 +10,6 @@ namespace librarymanagementArchitectureRepository.Repository
     class BookRepository
     {
         List<Book> GetAll(); // Method to get all books from the repository
+        Book GetById(string id); // Method to get a book by its unique identifier
     }
 }
