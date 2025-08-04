1d4615473b29c1569cb591130ae4aa3f71c77c8d
riham <riham91.alsiyabi@gmail.com>
2025-08-04 09:29:49 +0400

    File context for managing file operations

--- Repository/BorrowRecordRepository.cs
+++ Repository/BorrowRecordRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using librarymanagementArchitectureRepository.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,7 @@ namespace librarymanagementArchitectureRepository.Repository
 {
     class BorrowRecordRepository
     {
+        private readonly FileContext _context; // File context for managing file operations
+        private List<BorrowRecord> _records;
     }
 }
